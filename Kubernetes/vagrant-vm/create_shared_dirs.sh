44a64ac1166052c56d74808a567dcccb37ae6dac
Ben Koo <koo0905@gmail.com>
2021-03-21 16:41:29 +0800

    Added directory and script copying script create_shared_dirs.sh

---
+++ Kubernetes/vagrant-vm/create_shared_dirs.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+mkdir share
+mkdir share/master
+cp ../auto-installation/*.* ./share/master/.
+
+for i in {1..3}; do
+   mkdir ./share/node$i
+   cp ../auto-installation/*.* ./share/node$i/.
+done
